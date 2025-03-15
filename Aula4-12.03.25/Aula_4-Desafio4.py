66eebb3c1bf9257d1a727125a4ed2a12c6f76482
Gustav Hilbert <103065280+GusHilbert@users.noreply.github.com>
2025-03-14 21:17:54 -0300

    Add files via upload

--- Aula4-12.03.25/Aula_4-Desafio4.py
+++ Aula4-12.03.25/Aula_4-Desafio4.py
@@ -1,5 +1,5 @@
 numero = int(input("Coloque um número: "))%2
 if numero > 0 :
-    print(f"O número é ímpar")
+    print("O número é ímpar")
 else:
-    print(f"O número é par")
+    print("O número é par")
