6063fd41a29e67f5953536f1d7d2d784a823b2f0
Gustav Hilbert <103065280+GusHilbert@users.noreply.github.com>
2025-03-14 21:18:47 -0300

    Delete Aula5-12.03.25 directory

--- Aula5-12.03.25/Aula5-Desafio_01.py
+++
@@ -1,7 +0,0 @@
-idade = int(input("Qual é a sua idade? "))
-senha = input("Qual é a sua senha? ")
-
-if (idade >= 18) and (senha == "123456Ab!"):
-    print("Acesso aprovado")
-else:
-    print("Acesso negado")
--- Aula5-12.03.25/Aula5-Desafio_02.py
+++
@@ -1,7 +0,0 @@
-nota = int(input("Qual é a sua nota? "))
-trabalho = input("Houve um trabalho extra? (s/n) ")
-trabalho=trabalho.lower()
-if (nota >= 7) or (trabalho == "s"):
-    print("Aprovado")
-else:
-    print("Reprovado")
