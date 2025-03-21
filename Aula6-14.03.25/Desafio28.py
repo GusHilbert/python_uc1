dc33bfcbcc95d4793687d3085e3e48281b2d5f4f
Gustav Hilbert <103065280+GusHilbert@users.noreply.github.com>
2025-03-21 00:39:50 -0300

    Exercícios da aula 7

--- Aula6-14.03.25/Desafio28.py
+++ Aula6-14.03.25/Desafio28.py
@@ -23,7 +23,7 @@ while resposta != "4":
         3 - Enviar um erro
         4 - Saír
         """)
-        resposta = input("\nDigite o número relativo a sua solicitação: ")
+        resposta = input("Digite o número relativo a sua solicitação: ")
         exercicio.append(resposta)
     elif resposta =="2":
         print("Sua dúvida foi enviada")
@@ -34,10 +34,10 @@ while resposta != "4":
         3 - Enviar um erro
         4 - Saír
         """)
-        resposta = input("\nDigite o número relativo a sua solicitação: ")
+        resposta = input("Digite o número relativo a sua solicitação: ")
         duvida.append(resposta)
     elif resposta == "3":
-        print("Sua dúvida foi enviada")
+        print("Seu erro foi enviado")
         print("""
         Oque vc deseja fazer agora:
         1 - Enviar um exercício
@@ -45,27 +45,38 @@ while resposta != "4":
         3 - Enviar um erro
         4 - Saír
         """)
-        resposta = input("\nDigite o número relativo a sua solicitação: ")
+        resposta = input("Digite o número relativo a sua solicitação: ")
         erro.append(resposta)
 
-if len (exercicio) ==0 and len (duvida) == 0 and len(erro) == 0: 
-    print("""Desculpa não poder ter te ajudado =/
-          Até a próxima""")
-elif len (exercicio) !=0 and len (duvida) != 0 and len(erro) == 0: 
-    print("""Seu exercício e dúvida foram enviados com sucesso =)
-          Até a próxima""")
+if len(exercicio) ==0 and len(duvida) == 0 and len(erro) == 0: 
+    print("""
+Desculpa não poder ter te ajudado =/
+Até a próxima""")
+elif len(exercicio) !=0 and len(duvida) != 0 and len(erro) != 0: 
+    print("""
+Todas as suas solicitações foram enviadas!!!
+Até a próxima""")    
+elif len(exercicio) !=0 and len(duvida) != 0 and len(erro) == 0: 
+    print("""
+Seu exercício e dúvida foram enviados com sucesso =)
+Até a próxima""")
 elif len (exercicio) ==0 and len (duvida) != 0 and len(erro) != 0: 
-    print("""Sua dúvida e erro foram enviados com sucesso =)
-          Até a próxima""")
+    print("""
+Sua dúvida e erro foram enviados com sucesso =)
+Até a próxima""")
 elif len (exercicio) !=0 and len (duvida) == 0 and len(erro) != 0: 
-    print("""Seu exercício e erro foram enviados com sucesso =)
-          Até a próxima""")
+    print("""
+Seu exercício e erro foram enviados com sucesso =)
+Até a próxima""")
 elif len (exercicio) !=0 and len (duvida) == 0 and len(erro) == 0: 
-    print("""Seu exercício foi enviado com sucesso =)
-          Até a próxima""")
+    print("""
+Seu exercício foi enviado com sucesso =)
+Até a próxima""")
 elif len (exercicio) ==0 and len (duvida) != 0 and len(erro) == 0: 
-    print("""Sua dúvida foi enviada com sucesso =)
-          Até a próxima""")
+    print("""
+Sua dúvida foi enviada com sucesso =)
+Até a próxima""")
 elif len (exercicio) ==0 and len (duvida) == 0 and len(erro) != 0: 
-    print("""Seu erro foi enviado com sucesso =)
-          Até a próxima""")
+    print("""
+Seu erro foi enviado com sucesso =)
+Até a próxima""")
